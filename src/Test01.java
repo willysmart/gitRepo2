73ede657b7de8b952a254c9211262d41efedf227
willysmart <willysmart@hotmail.com>
2020-07-03 16:50:07 -0400

    update test01.java

--- src/Test01.java
+++ src/Test01.java
@@ -1,6 +1,6 @@
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("Test01.main");
-        System.out.println("branch03");
+        System.out.println("master");
     }
 }
