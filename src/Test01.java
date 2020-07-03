29c96b577ac685d389b700b7b6964a695fc6c01f
willysmart <willysmart@users.noreply.github.com>
2020-07-03 17:05:54 -0400

    Revert "update test01.java"
    This reverts commit 92f054aa

--- src/Test01.java
+++ src/Test01.java
@@ -2,6 +2,5 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Test01.main");
         System.out.println("branch03");
-        System.out.println("branch03_new");
     }
 }
